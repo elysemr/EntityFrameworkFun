1bcdd0779a47b4d47045c92be53230b0bd2c74d9
Elyse Roth <elysemorganroth@gmail.com>
2021-10-05 15:57:19 -0400

    Add project files.

---
+++ EntityFrameworkFun/Models/ClassesForMajorIncInstructor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EntityFrameworkFun.Models
+{
+    public partial class ClassesForMajorIncInstructor
+    {
+        public string Majorcod { get; set; }
+        public string Major { get; set; }
+        public string Name { get; set; }
+        public string Class { get; set; }
+    }
+}
---
+++ EntityFrameworkFun/Program.cs
@@ -0,0 +1,21 @@
+﻿using EntityFrameworkFun.Models;
+using System;
+using System.Linq;
+
+namespace EntityFrameworkFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _context = new EdDbContext();
+            //now can use the DB
+            var majors = _context.Majors.ToList();
+            foreach(var m in majors)
+            {
+                Console.WriteLine($"{m.Description}");
+            }
+
+        }
+    }
+}
