bd76ae594e2c3e7697772b6dd88a4d37744cb48a
Elyse Roth <elysemorganroth@gmail.com>
2021-10-06 12:05:57 -0400

    added get all, get by pk, and created new student with students controller

--- EntityFrameworkFun/Program.cs
+++ EntityFrameworkFun/Program.cs
@@ -8,85 +8,109 @@ namespace EntityFrameworkFun
     class Program
     {
         static void Main(string[] args)
-        { 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        {
+            Student student = null;
+            var studentsCtrl = new StudentsController();
+
+            var NewStudent = new Student()
+            {
+                Id = 0,
+                Firstname = "Elyse",
+                Lastname = "Roth",
+                StateCode = "NY",
+                Sat = 1600,
+                Gpa = 4.0m,
+                MajorId = null
+            };
+            studentsCtrl.Create(NewStudent);
+
+
+            var students = studentsCtrl.GetAll();
+            foreach (var s in students)
+            {
+                Console.WriteLine($"{s.Firstname} {s.Lastname} | {s.StateCode} | {s.Sat}");
+            }
+
+            //student = studentsCtrl.GetByPk(44);
+            //Console.WriteLine($"{student.Firstname} {student.Lastname} | {student.Sat} | {student.Gpa}");
+
+
+
+
         }
 
-        static void X() { 
-            Major major = null; //declare var so don't have to keep declaring
-            //display all majors in major table
-            var majorsCtrl = new MajorsController();
+        static void X()
+            {
+                Major major = null; //declare var so don't have to keep declaring
+                                    //display all majors in major table
+                var majorsCtrl = new MajorsController();
 
-            var NewMajor = new Major()
-            {
-                Id = 0, Code = "MUSC", Description = "Music", MinSat = 1595  //anything we don't give a value will default, null
-            };
-            try
-            {
-                var rc1 = majorsCtrl.Create(NewMajor); //(rc =return code)
-                if (!rc1)
+                var NewMajor = new Major()
                 {
-                    Console.WriteLine($"Create new major failed.");
+                    Id = 0,
+                    Code = "MUSC",
+                    Description = "Music",
+                    MinSat = 1595  //anything we don't give a value will default, null
+                };
+                try
+                {
+                    var rc1 = majorsCtrl.Create(NewMajor); //(rc =return code)
+                    if (!rc1)
+                    {
+                        Console.WriteLine($"Create new major failed.");
+                    }
                 }
-            } catch (Exception ex)
-            {
-                Console.WriteLine($"Exception occurred: {ex.Message}");
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Exception occurred: {ex.Message}");
+                }
+
+                NewMajor.Description = "Classical Music";
+                majorsCtrl.Change(NewMajor.Id, NewMajor);
+
+                var rc = majorsCtrl.Remove(NewMajor.Id);
+                if (!rc)
+                {
+                    Console.WriteLine($"Remove major failed.");
+                }
+
+
+                major = majorsCtrl.GetByCode("MATH");
+                Console.WriteLine(major); //now this contains and instance of major from major class and will cw that from class
+                                          //if (major.Code == null)
+                                          //{
+                                          //    Console.WriteLine("Major not found.");
+                                          //} else 
+                                          //Console.WriteLine($"{major.Code}");
+
+                major = majorsCtrl.GetByPk(4);
+                Console.WriteLine($"{major.Description}");
+
+
+                var majors = majorsCtrl.GetAll();
+
+                foreach (var m in majors)
+                {
+                    Console.WriteLine($"{m.Description}");
+                }
+
+
+                //if want to make exception or cw for something not there
+
+                //var majorsCtrl = new MajorsController();
+                //var major = majorsCtrl.GetByPk(44444);
+                //if (major == null)
+                //{
+                //    Console.WriteLine("Not Found.");
+                //}
+                //Console.WriteLine($"{major.Description}");
+
+
+
+
+
+
             }
-
-            NewMajor.Description = "Classical Music";
-            majorsCtrl.Change(NewMajor.Id, NewMajor);
-
-           var rc = majorsCtrl.Remove(NewMajor.Id);
-            if (!rc)
-            {
-                Console.WriteLine($"Remove major failed.");
-            }
-      
-
-    major = majorsCtrl.GetByCode("MATH");
-            Console.WriteLine(major); //now this contains and instance of major from major class and will cw that from class
-            //if (major.Code == null)
-            //{
-            //    Console.WriteLine("Major not found.");
-            //} else 
-            //Console.WriteLine($"{major.Code}");
-
-            major = majorsCtrl.GetByPk(4);
-            Console.WriteLine($"{major.Description}");
-
-
-            var majors = majorsCtrl.GetAll();
-
-            foreach (var m in majors)
-            {
-                Console.WriteLine($"{m.Description}");
-            }
-
-
-            //if want to make exception or cw for something not there
-
-            //var majorsCtrl = new MajorsController();
-            //var major = majorsCtrl.GetByPk(44444);
-            //if (major == null)
-            //{
-            //    Console.WriteLine("Not Found.");
-            //}
-            //Console.WriteLine($"{major.Description}");
-
-
-
-
-
-
         }
     }
-}
+
